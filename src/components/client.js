c8748f9a25d9d979a8b2f2503f20159da4f8d535
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-10-23 20:47:33 -0300

    created a main section and a mid circle and moved 5 sumoners from blue side to red side

--- src/components/client.js
+++ src/components/client.js
@@ -1,122 +1,141 @@
 import React from "react";
 
 export default function Client(){
-return(
-    <div className="client">
-        
-            <div className="top-part">
-            <ul className="bans-blueside">
-                <li>
-                    <div className="ban-example"></div>
-                </li>
-                <li>
-                    <div className="ban-example"></div>
-                </li>
-                <li>
-                    <div className="ban-example"></div>
-                </li>
-                <li>
-                    <div className="ban-example"></div>
-                </li>
-                <li>
-                    <div className="ban-example"></div>
-                </li>
-            </ul>
-            <ul className="bans-blueside">
-                <li>
-                    <div className="ban-example"></div>
-                </li>
-                <li>
-                    <div className="ban-example"></div>
-                </li>
-                <li>
-                    <div className="ban-example"></div>
-                </li>
-                <li>
-                    <div className="ban-example"></div>
-                </li>
-                <li>
-                    <div className="ban-example"></div>
-                </li>
-            </ul>
-            </div>
-
-
-        
-        <div className="blueside-team">
-         <div className="champion-player">
-         <div className="champion-player-logo"></div>
-        <ul className="champion-player-text">
-            <li>Champion</li>
-            <li>Role</li>
-            <li>Sumoner Name</li>
-        </ul>
-        </div>
-        <div className="champion-player">
-        <div className="champion-player-logo"></div>
-        <ul className="champion-player-text">
-            <li>Champion</li>
-            <li>Role</li>
-            <li>Sumoner Name</li>
-        </ul>
-        </div>
-        <div className="champion-player">
-        <div className="champion-player-logo"></div>
-        <ul className="champion-player-text">
-            <li>Champion</li>
-            <li>Role</li>
-            <li>Sumoner Name</li>
-        </ul>
-        </div>
-        <div className="champion-player">
-        <div className="champion-player-logo"></div>
-        <ul className="champion-player-text">
-            <li>Champion</li>
-            <li>Role</li>
-            <li>Sumoner Name</li>
-        </ul>
-        </div>
-        </div>
-
-        
-        <div className="redside-team">
-         <div className="champion-player">
-         <div className="champion-player-logo"></div>
-        <ul className="champion-player-text">
-            <li>Champion</li>
-            <li>Role</li>
-            <li>Sumoner Name</li>
-        </ul>
-        </div>
-        <div className="champion-player">
-        <div className="champion-player-logo"></div>
-        <ul className="champion-player-text">
-            <li>Champion</li>
-            <li>Role</li>
-            <li>Sumoner Name</li>
-        </ul>
-        </div>
-        <div className="champion-player">
-        <div className="champion-player-logo"></div>
-        <ul className="champion-player-text">
-            <li>Champion</li>
-            <li>Role</li>
-            <li>Sumoner Name</li>
-        </ul>
-        </div>
-        <div className="champion-player">
-        <div className="champion-player-logo"></div>
-        <ul className="champion-player-text">
-            <li>Champion</li>
-            <li>Role</li>
-            <li>Sumoner Name</li>
-        </ul>
-        </div>
-        </div>
-
-
-
-        {/*code above this*/ }
+return (
+  <div className="client">
+    <div className="top-part">
+      <ul className="bans-blueside">
+        <li>
+          <div className="ban-example"></div>
+        </li>
+        <li>
+          <div className="ban-example"></div>
+        </li>
+        <li>
+          <div className="ban-example"></div>
+        </li>
+        <li>
+          <div className="ban-example"></div>
+        </li>
+        <li>
+          <div className="ban-example"></div>
+        </li>
+      </ul>
+      <ul className="bans-blueside">
+        <li>
+          <div className="ban-example"></div>
+        </li>
+        <li>
+          <div className="ban-example"></div>
+        </li>
+        <li>
+          <div className="ban-example"></div>
+        </li>
+        <li>
+          <div className="ban-example"></div>
+        </li>
+        <li>
+          <div className="ban-example"></div>
+        </li>
+      </ul>
     </div>
-)
+
+    <main>
+      <div className="blueside-team">
+        <div className="champion-player">
+          <div className="champion-player-logo"></div>
+          <ul className="champion-player-text">
+            <li>Champion</li>
+            <li>Role</li>
+            <li>Sumoner Name</li>
+          </ul>
+        </div>
+        <div className="champion-player">
+          <div className="champion-player-logo"></div>
+          <ul className="champion-player-text">
+            <li>Champion</li>
+            <li>Role</li>
+            <li>Sumoner Name</li>
+          </ul>
+        </div>
+        <div className="champion-player">
+          <div className="champion-player-logo"></div>
+          <ul className="champion-player-text">
+            <li>Champion</li>
+            <li>Role</li>
+            <li>Sumoner Name</li>
+          </ul>
+        </div>
+        <div className="champion-player">
+          <div className="champion-player-logo"></div>
+          <ul className="champion-player-text">
+            <li>Champion</li>
+            <li>Role</li>
+            <li>Sumoner Name</li>
+          </ul>
+        </div>
+        <div className="champion-player">
+          <div className="champion-player-logo"></div>
+          <ul className="champion-player-text">
+            <li>Champion</li>
+            <li>Role</li>
+            <li>Sumoner Name</li>
+          </ul>
+        </div>
+      </div>
+      <div className="main-mid">
+        <div className="current-stage">
+            <p>Current Thing</p>
+            <p>99</p>
+        </div>
+        <div className="main-circle">
+        </div>
+      </div>
+      <div className="redside-team">
+      <div className="champion-player">
+          <div className="champion-player-logo"></div>
+          <ul className="champion-player-text">
+            <li>Champion</li>
+            <li>Role</li>
+            <li>Sumoner Name</li>
+          </ul>
+        </div>
+        <div className="champion-player">
+          <div className="champion-player-logo"></div>
+          <ul className="champion-player-text">
+            <li>Champion</li>
+            <li>Role</li>
+            <li>Sumoner Name</li>
+          </ul>
+        </div>
+        <div className="champion-player">
+          <div className="champion-player-logo"></div>
+          <ul className="champion-player-text">
+            <li>Champion</li>
+            <li>Role</li>
+            <li>Sumoner Name</li>
+          </ul>
+        </div>
+        <div className="champion-player">
+          <div className="champion-player-logo"></div>
+          <ul className="champion-player-text">
+            <li>Champion</li>
+            <li>Role</li>
+            <li>Sumoner Name</li>
+          </ul>
+        </div>
+        <div className="champion-player">
+          <div className="champion-player-logo"></div>
+          <ul className="champion-player-text">
+            <li>Champion</li>
+            <li>Role</li>
+            <li>Sumoner Name</li>
+          </ul>
+        </div>
+      </div>
+    </main>
+
+    {/*code above this*/}
+  </div>
+);
 }
