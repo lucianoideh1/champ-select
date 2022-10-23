ad4014dd5073191aaa6d29fdb8a2b12e18ab978b
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-10-23 18:47:17 -0300

    created the client component and created ban slots and some champ summonmer thing

---
+++ src/components/client.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default function Client(){
+return(
+    <div className="client">
+        
+            <div className="top-part">
+            <ul className="bans-blueside">
+                <li>
+                    <div className="ban-example"></div>
+                </li>
+                <li>
+                    <div className="ban-example"></div>
+                </li>
+                <li>
+                    <div className="ban-example"></div>
+                </li>
+                <li>
+                    <div className="ban-example"></div>
+                </li>
+                <li>
+                    <div className="ban-example"></div>
+                </li>
+            </ul>
+            <ul className="bans-blueside">
+                <li>
+                    <div className="ban-example"></div>
+                </li>
+                <li>
+                    <div className="ban-example"></div>
+                </li>
+                <li>
+                    <div className="ban-example"></div>
+                </li>
+                <li>
+                    <div className="ban-example"></div>
+                </li>
+                <li>
+                    <div className="ban-example"></div>
+                </li>
+            </ul>
+            </div>
+
+        <div className="champion-player">
+        <div className="champion-player-logo"></div>
+        <span>Champion Name</span>
+        <p>Summoner Name</p>
+        </div>
+    </div>
+)
+}
